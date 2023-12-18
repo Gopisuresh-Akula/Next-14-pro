4b79a3472f52686f27d482198bba6c6207fd751f
Gopisuresh-Akula <gopisureshakula123@gmail.com>
2023-12-18 22:48:24 +0530

    feat: create page.js

---
+++ src/app/profile/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProfilePage = () => {
+    return (
+        <div>
+            <h1>Profile Page</h1>
+            {/* Your profile page content goes here */}
+        </div>
+    );
+};
+
+export default ProfilePage;
